f18a4f0afbd253032022533e4aa061292b83841d
Lydia Farag <77176132+lydiafarag@users.noreply.github.com>
2022-06-25 17:28:45 -0400

    Enter

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from peewee import*
 
 load_dotenv()
 app = Flask(__name__ )
-mydb =MySQLDatabase(os,getenv("MYSQL_DATABASE"),
+mydb =MySQLDatabase(os.getenv("MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv("MYSQL_PASSWORD"),
     host=os.getenv("MYSQL_HOST"),
