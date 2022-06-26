39ba03aec34c211cda2dc92a779bbf064f65624f
Lydia Farag <77176132+lydiafarag@users.noreply.github.com>
2022-06-26 16:48:48 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #here we kill any existing tmux sessions
-tmux kill-server
+#tmux kill-server
 #enter into the project
 cd project-best-project
 git fetch && git reset origin/main --hard
@@ -10,7 +10,9 @@ source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
 #starting flask in a detached tmux session
-tmux new-session -d -s Flask
-tmux send-keys 'source python3-virtualenv/bin/activate' C-m
-tmux send-keys 'flask run --host=0.0.0.0' C-m
-tmux detach -s Flask
+#tmux new-session -d -s Flask
+#tmux send-keys 'source python3-virtualenv/bin/activate' C-m
+#tmux send-keys 'flask run --host=0.0.0.0' C-m
+#tmux detach -s Flask
+systemctl daemon-reload
+systemctl restart myportfolio
